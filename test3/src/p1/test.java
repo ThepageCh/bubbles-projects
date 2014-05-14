36c855fd3a4316e37572885603ff3bca76d3af54
ThepageCh <leon.luethi@gmail.com>
2014-05-14 13:41:13 +0200

    MessagePane

--- test3/src/p1/test.java
+++ test3/src/p1/test.java
@@ -1,8 +1,11 @@
 package p1;
 
+import javax.swing.JOptionPane;
+
 public class test {
 	public static void main(String[] args) {
 		System.out.println("hallo");
 		//hallo
+		JOptionPane.showMessageDialog(null, "hoi");
 	}
 }
